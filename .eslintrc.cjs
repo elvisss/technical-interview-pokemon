2e1ae4fc2dc322ea5756dc81c99ec957bb9b98f6
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-04-12 13:03:50 -0500

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': 'off',
+    'react/prop-types': 'off'
+  }
+}
